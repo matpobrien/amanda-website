bc4858bfbb2dd6caec36a8cb85090fa498801de3
Matthew Paul O'Brien <matpobrien@gmail.com>
2022-04-10 17:05:22 +0200

    frontend: divide navigation system into more components and correct event bubbling

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,36 +2,27 @@ import React, { Component } from "react";
 import NavButton from "./NavButton";
 
 class Navigation extends Component {
-    setDisplay =(childSection) => {
-        this.props.parentCallback(childSection)
-    };
-
+    state = {
+        sections: ['About', 'Coaching', 'FAQ', 'Contact'],
+    }
     render() {
+        const props = this.props;
+        function setDisplay(childSection) { props.parentCallback(childSection) };
         return (
             <div className="navigation">
-                <NavButton
-                    parentCallback={this.setDisplay}
-                    name='About'
-                    selected={this.props.currentDisplay}
-                />
-                <NavButton
-                    parentCallback={this.setDisplay}
-                    name='Coaching'
-                    selected={this.props.currentDisplay}
-                />
-                <NavButton
-                    parentCallback={this.setDisplay}
-                    name='FAQ'
-                    selected={this.props.currentDisplay}
-                />
-                <NavButton
-                    parentCallback={this.setDisplay}
-                    name='Contact'
-                    selected={this.props.currentDisplay}
-                />
+               {
+               this.sectionMenu = this.state.sections.map(function(section, index)
+                {
+                return <NavButton
+                        key={index}
+                        parentCallback={setDisplay}
+                        name={section}
+                        selected={props.currentDisplay}
+                        />
+               })};
             </div>
-            );
-    }
-}
+        );
+    };
 
-export default Navigation
+}
+export default Navigation;
